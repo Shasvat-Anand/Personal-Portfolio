4b7d58329ff87490032a07e5fc1c1564079b144e
Shasvat-Anand <anandk84a@gmail.com>
2025-10-09 09:30:40 +0530

    header file upload

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Header from './components/header/header';
+import Home from './components/home/home';
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+    <Header/>
+    <Home/>
+    </>
+  )
+}
+
+export default App
