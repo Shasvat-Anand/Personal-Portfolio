731a91a45725acd6966986618c78954d365bdf64
Shasvat-Anand <anandk84a@gmail.com>
2025-10-09 22:16:56 +0530

    add section for home page

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import Social from './Social'
+import Data from './Data'
+import ScrollDown from './ScrollDown'
+import "./home.css"
 
-const home = () => {
+const Home = () => {
   return (
-    <div>home</div>
+    <section className="home section" id="home">
+      <div className="home__container container grid">
+        <div className="home__content grid">
+          <Social/>
+          <div className="home__img"></div>
+          <Data/>
+        </div>
+          <ScrollDown/>
+      </div>
+    </section>
   )
 }
 
-export default home
+export default Home
